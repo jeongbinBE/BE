71d64b545bb207de3179443977c6ea8fa3fd9684
Jeongbin <jeongbinkim@bigelephant.co.kr>
2015-03-30 01:55:45 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigElephant::Application.config.secret_key_base = 'a11c39dbd38fbd75a1369d6c5a31d380b3d149991eb935eb3db5dbe389cdfd8cfd129ad871325234191e1e02a7e729772594afa2d891066a8730cd9494bd42aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BigElephant::Application.config.session_store :cookie_store, key: '_BigElephant_session'
